c9aa65a38a6a8858e0c82e92b77c94cd9c66a3ff
VijayKumarDVK96 <43247564+VijayKumarDVK96@users.noreply.github.com>
2023-01-05 08:45:18 +0530

    updated readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Calculator with Javascript
+
+**DEMO :** https://tiny-melomakarona-d6b689.netlify.app/
+
+Developed a responsive website for Simple Calculator using Javascript where calculations like add, sub, multiply, divide can be performed.
+
+> **Technologies used:** HTML, CSS, JS
+
+# Pseudo Code
+
+ - Create a function, if first inputted numbers display in the screen and coming up next append it to the screen.
+ - Clear button - Reset the screen to 0 and input value to 0.
+ - By clicking equal, will evaluate the arithmetic operations, if it is not a number or infinity then should display as Math error, else if the result has more decimal numbers round off maximum to 15 and display to the screen, else display the result to the screen.
+ - Delete button delete the last digit of the displayed number and append it back to the screen.
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let display = document.getElementById('result');
 let input = 0;
 
+/*
+01 - Create a function, if first inputted numbers display in the screen and coming up next append it to the screen
+*/
+
 function type(el) {
     if (input == 0) {
         input = el.innerText;
@@ -11,10 +15,18 @@ function type(el) {
     display.innerText = input;
 }
 
+/*
+02 - Clear button - Reset the screen to 0 and input value to 0
+*/
+
 document.getElementById("clear").addEventListener('click', function () {
     display.innerText = 0;
     input = 0;
 });
+
+/*
+03 - By clicking equal, will evaluate the arithmetic operations, if it is not a number or infinity then should display as Math error, else if the result has more decimal numbers round off maximum to 15 and display to the screen, else display the result to the screen
+*/
 
 document.getElementById("equal").addEventListener('click', function () {
     let result = 0;
@@ -30,6 +42,10 @@ document.getElementById("equal").addEventListener('click', function () {
 
     input = result;
 });
+
+/*
+04 - Delete button delete the last digit of the displayed number and append it back to the screen
+*/
 
 document.getElementById("delete").addEventListener('click', function () {
     display = document.getElementById('result');
